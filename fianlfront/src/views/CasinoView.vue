25ae03bd4b52540268231dae29ba9c1df351e7cf
seosanggyun <seosanggyun@naver.com>
2022-05-24 15:49:38 +0900

    casino view modified

--- fianlfront/src/views/CasinoView.vue
+++ fianlfront/src/views/CasinoView.vue
@@ -4,34 +4,94 @@
 
     <h1>CASINO</h1>
     <br>
+
     <b-button 
-    v-b-modal.modal-xl
+    v-b-modal.modal-quiz
     variant="primary"
-    @click="[fetchQuiz(), putQuiz(), pickAnswer()]">
+    @click="[fetchQuiz(), putQuiz(), pickAnswer(), notificate('1000 포인트를 사용하여 퀴즈를 시작합니다!')]">
         QUIZ
     </b-button>
-    <b-modal id="modal-xl" size="xl" title="Quiz">
+    <b-modal id="modal-quiz" hide-footer hide-header size="xl" title="QUIZ">
     <img :src="`http://image.tmdb.org/t/p/original/${this.pickedAnswer.backdrop_path}`">
       <!-- {{ pickAnswer.backdrop_path }} -->
-    <p 
+    <div
     v-for="quiz in quizs" :key="quiz.pk">
-      <b-button>
-        {{ quiz.title }}
-      </b-button>
-    </p>
+      <div v-if="quiz.pk === pickedAnswer.pk">
+        <b-button class="mt-3" variant="outline-danger" block @click="[fetchRaisePoint(), $bvModal.hide('modal-quiz'), notificate('정답입니다! 10000포인트 추가!')]">
+          {{ quiz.title }}
+        </b-button>
+      </div>
+      <div v-else>
+        <b-button  class="mt-3" variant="outline-danger" block @click="[$bvModal.hide('modal-quiz'), notificate('오답ㅠㅠ')]">
+          {{ quiz.title }}
+        </b-button>
+      </div>
+    </div>
     </b-modal>
     <br>
-    <router-link :to="{ name: 'backGacha'}">
-      BACK GACHA
-    </router-link>
+
+    <b-button 
+    v-b-modal.modal-back
+    variant="success"
+    @click="[fetchBack(), pickBack(), notificate('1000 포인트를 사용하여 배경화면을 뽑았습니다!')]">
+        BACK GACHA
+    </b-button>
+    <b-modal id="modal-back" hide-footer hide-header size="xl" title="BACKGACHA">
+    <img :src="`http://image.tmdb.org/t/p/original/${this.pickedBack.backdrop_path}`">
+      <p>
+        {{ pickedBack.title }}
+      </p>
+      <b-button class="mt-3" variant="primary" block @click="[fetchStoreBack(pickedBack.pk), $bvModal.hide('modal-back'), notificate('프로필 배경사진이 바뀌었어요!')]">
+          내 프로필 배경 바꾸기
+      </b-button>
+      <b-button class="mt-3" variant="danger" block @click="[$bvModal.hide('modal-back'), notificate('이걸 버리네!')]">
+          갖다 버리기
+      </b-button>
+    </b-modal>
+
     <br>
-    <router-link :to="{ name: 'cardGacha' }">
-      CARD GACHA
-    </router-link>
+
+    <b-button 
+    v-b-modal.modal-card
+    variant="warning"
+    @click="[fetchCard(), pickCard(), fetchStoreCard(pickedCard.pk), notificate('1000포인트를 사용하여 카드 1장을 뽑았습니다!')]">
+        CARD GACHA
+    </b-button>
+    <b-modal id="modal-card" hide-footer hide-header size="sm" title="CARDGACHA">
+      <div class="card" :class="{'platinum': pickedCard.popularity >= 40, 'gold': pickedCard.popularity < 40, 'silver': pickedCard.popularity < 21, 'bronze': pickedCard.popularity < 14}">
+        <div class="card-header">
+            </div>
+            <img class="card-img" :src="`http://image.tmdb.org/t/p/original/${pickedCard.profile_path}`">
+            <div class="card-body">
+              <h5 class="card-title">{{ pickedCard.name }}</h5>
+            </div>
+      </div>
+    </b-modal>
+
     <br>
-    <router-link :to="{ name: 'cardGachaTen' }">
-      CARD GACHA TEN
-    </router-link>
+
+    <b-button 
+    v-b-modal.modal-card-ten
+    variant="danger"
+    @click="[fetchCards(), pickCards(), fetchStoreCard(pickedCards[0].pk), fetchStoreCard(pickedCards[1].pk), fetchStoreCard(pickedCards[2].pk), fetchStoreCard(pickedCards[3].pk), fetchStoreCard(pickedCards[4].pk), fetchStoreCard(pickedCards[5].pk), fetchStoreCard(pickedCards[6].pk), fetchStoreCard(pickedCards[7].pk), fetchStoreCard(pickedCards[8].pk), fetchStoreCard(pickedCards[9].pk), fetchStoreCard(pickedCards[10].pk),fetchStoreCard(pickedCards[11].pk),notificate('10000포인트를 사용하여 카드 12장을 뽑았습니다!')]">
+        CARD GACHA TEN
+    </b-button>
+    <b-modal id="modal-card-ten" hide-footer hide-header size="lg" title="CARDGACHATEN">
+      <div class="card-deck container">
+        <div class="row">
+          <div class="card col-3" :class="{'platinum': card.popularity >= 40, 'gold': card.popularity < 40, 'silver': card.popularity < 21, 'bronze': card.popularity < 14}" v-for="card in pickedCards" :key="card.pk">
+            <div class="card-header">
+            </div>
+            <img class="card-img" :src="`http://image.tmdb.org/t/p/original/${card.profile_path}`">
+            <div class="card-body">
+              <h5 class="card-title">{{ card.name }}</h5>
+            </div>
+        </div>
+        </div>
+    
+
+      </div>
+    </b-modal>
 
 
   </div>
@@ -52,34 +112,69 @@ export default {
   data(){
     return {
       quizs: [],
-      pickedAnswer: {}
+      pickedAnswer: {},
+      pickedBack: {},
+      pickedCard: {},
+      pickedCards: [],
     }
   },
   computed: {
-    // ...mapGetters(['quiz', 'raisePoint', 'back', 'storeBack', 'card', 'cards', 'storeCard'])
-    ...mapGetters(['quiz']),
+    ...mapGetters(['quiz', 'raisePoint', 'back', 'storeBack', 'card', 'cards', 'storeCard']),
   },
   methods: {
-    // ...mapActions(['fetchQuiz', 'fetchRaisePoint', 'fetchBack', 'fetchStoreBack', 'fetchCard', 'fetchCards', 'fetchStoreCard'])
-    ...mapActions(['fetchQuiz']),
+    ...mapActions(['fetchQuiz', 'fetchRaisePoint', 'fetchBack', 'fetchStoreBack', 'fetchCard', 'fetchCards', 'fetchStoreCard']),
+    notificate(message) {
+      alert(message)
+    },
+    storecards(personPk){
+      this.fetchStoreCard(personPk)
+    },
     putQuiz() {
       this.quizs = this.quiz
     },
     pickAnswer() {
       this.pickedAnswer = _.sample(this.quizs)
     },
+    pickBack() {
+      this.pickedBack = this.back[0]
+    },
+    pickCard() {
+      this.pickedCard = this.card[0]
+    },
+    pickCards() {
+      this.pickedCards = this.cards
+    }
 
 
   },
   created() {
-    // this.fetchQuiz()
-    // this.fetchBack()
-    // this.fetchCard()
-    // this.fetchCards()
+    this.fetchQuiz()
+    this.fetchBack()
+    this.fetchCard()
+    this.fetchCards()
   }
 }
 </script>
 
 <style>
+  .platinum {
+    background: rgb(248,255,160);
+    background: linear-gradient(0deg, rgba(248,255,160,1) 0%, rgba(181,251,255,1) 19%, rgba(255,158,253,1) 80%, rgba(253,255,199,1) 100%);
+  }
+  .gold {
+    background: rgb(249,242,149);
+    background: linear-gradient(0deg, rgba(249,242,149,1) 0%, rgba(224,170,62,1) 23%, rgba(249,242,149,1) 79%, rgba(184,138,68,1) 100%);
+  }
+  .silver {
+    background: rgb(232,232,232);
+    background: linear-gradient(0deg, rgba(232,232,232,1) 0%, rgba(175,175,175,1) 23%, rgba(236,236,236,1) 79%, rgba(159,159,159,1) 100%);
+  }
+  .bronze {
+    background: rgb(255,220,180);
+    background: linear-gradient(0deg, rgba(255,220,180,1) 0%, rgba(120,61,12,1) 23%, rgba(255,222,161,1) 79%, rgba(120,61,12,1) 100%);
+  }
+  .modal-backdrop {
+   background-color: darkolivegreen;
+}
 
 </style>
