50d1290b08bcf4b13d7c15990119b3a043aa4d39
seosanggyun <seosanggyun@naver.com>
2022-05-24 21:16:27 +0900

    0524 before merge

--- fianlfront/src/views/MainView.vue
+++ fianlfront/src/views/MainView.vue
@@ -4,7 +4,7 @@
     <h1>main</h1>
     <a href="/logout">logout</a>
     <br>
-    <router-link :to="{path: `/profile`}">
+    <router-link :to="{ path: `/profile` }">
       <button>Profile</button>
     </router-link> | 
     <router-link :to="{ name: 'community' }">
@@ -16,28 +16,29 @@
     <h1>추천영화</h1>
     <br>
     <h3>판타지</h3>
-    <div class="movieitem">
-      <carousel v-if="filteredFantasyMovies" key="new_a" :autoplay="true" :nav="false" :dots="true" class="marginTop50 movieitem" :number="4" >
+    <!-- <div class="movieitem" v-if="filteredFantasyMovies" key="new_a">
+      <carousel :autoplay="true" :nav="false" :dots="true" class="marginTop50 movieitem" :number="4" >
         <div v-for="movie in filteredFantasyMovies" :key="movie.pk" >
           <a :href="`/movie/${movie.pk}`">
             <img :src='`http://image.tmdb.org/t/p/original${movie.poster_path}`' alt="movieposter">
           </a>
         </div>
-      </carousel>
-    </div>
+      </carousel> -->
+    <!-- </div> -->
     <br>
     <h3>범죄</h3>
-    <carousel v-if="filteredCrimeMovies" key="new_b" :autoplay="true" :nav="false" :dots="true" class="marginTop50 movieitem" :number="4" >
+    <!-- <carousel v-if="filteredCrimeMovies" key="new_b" :autoplay="true" :nav="false" :dots="true" class="marginTop50 movieitem" :number="4" >
       <div v-for="movie in filteredCrimeMovies" :key="movie.pk" >
         <a :href="`/movie/${movie.pk}`">
           <img :src='`http://image.tmdb.org/t/p/original${movie.poster_path}`' alt="movieposter">
         </a>
       </div>
-    </carousel>
+    </carousel> -->
     <br>
     <h3>코미디</h3>
-    <carousel v-if="filteredComdeyMovies" key="new_c" :autoplay="true" :nav="false" :dots="true" class="marginTop50 movieitem" :number="4" >
-      <div v-for="movie in filteredComedyMovies" :key="movie.pk" >
+    <button @click="[putComedyMovies()]">되냐</button>
+    <carousel  :autoplay="true" :nav="false" :dots="true" class="marginTop50 movieitem" :number="4" >
+      <div v-for="movie in comedyMovies" :key="movie.pk" >
         <a :href="`/movie/${movie.pk}`">
           <img :src='`http://image.tmdb.org/t/p/original${movie.poster_path}`' alt="movieposter">
         </a>
@@ -45,13 +46,13 @@
     </carousel>
     <br>
     <h3>SF</h3>
-    <carousel v-if="filteredSFMovies" key="new_d" :autoplay="true" :nav="false" :dots="true" class="marginTop50 movieitem" :number="4" >
+    <!-- <carousel v-if="filteredSFMovies" key="new_d" :autoplay="true" :nav="false" :dots="true" class="marginTop50 movieitem" :number="4" >
       <div v-for="movie in filteredSFMovies" :key="movie.pk" >
         <a :href="`/movie/${movie.pk}`">
           <img :src='`http://image.tmdb.org/t/p/original${movie.poster_path}`' alt="movieposter">
         </a>
       </div>
-    </carousel>
+    </carousel> -->
     
   </div>
 </template>
@@ -74,6 +75,11 @@ export default {
     NavbarItem,
     MovieSearchForm,
     carousel,
+  },
+  data(){
+    return {
+      comedyMovies: [],
+    }
   },
   computed:{
     ...mapGetters(['currentUser', 'movies']),
@@ -108,10 +114,14 @@ export default {
   },
   methods: {
     ...mapActions(['fetchCurrentUser','fetchProfile','fetchMovies']),
+    putComedyMovies() {
+        this.comedyMovies = this.filteredComedyMovies
     },
+  },
   created() {
     this.fetchCurrentUser()
     this.fetchMovies()
+    // this.putComedyMovies()
   },
 }
 </script>
